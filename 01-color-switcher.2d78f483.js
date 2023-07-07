282f75f627a85fc1b676189612060934cec655e8
IndianaCornell <91377811+IndianaCornell@users.noreply.github.com>
2023-07-07 21:16:04 +0000

    Deploying to gh-pages from @ IndianaCornell/goit-js-hw-09@7d1e0bccb36bf2b653f987915c5cf78992ca2edb 🚀

---
+++ 01-color-switcher.2d78f483.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;t.addEventListener("click",(function(){e.disabled&&(e.disabled=!1);a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(a),e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.2d78f483.js.map
